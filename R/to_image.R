0f744fe926ced429f8784d4a5547066086274ec8
John Hinkle <john.hinkle@utexas.edu>
2025-04-24 11:55:01 -0500

    to_image()

--- R/to_image.R
+++ R/to_image.R
@@ -1,4 +1,4 @@
-to_image <- function(data, column_name){
-  dir.create("images")
-  dpylr::mutate(data, ImagePath = paste0("images/", column_name, ".png"))
+to_image <- function(data, column_name = "state", new_dir = "images"){
+  dir.create(new_dir)
+  dplyr::mutate(data, ImagePath = paste0(new_dir, "/", .data[[column_name]], ".png"))
 }
