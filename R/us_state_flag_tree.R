fc46ce483630c08f2db4067e77eb94ee99dd0ecf
John Hinkle <john.hinkle@utexas.edu>
2025-04-28 10:48:30 -0500

    tip labeling

--- R/us_state_flag_tree.R
+++ R/us_state_flag_tree.R
@@ -1,4 +1,4 @@
-us_state_flag_tree <- function(tree, metadata, image_column = "ImagePath", label_column = "label") {
+us_state_flag_tree <- function(tree, metadata, image_column = "image_path", label_column = "label") {
 
   # create ggtree object from tree object
   p <- ggtree(tree)
@@ -10,7 +10,7 @@ us_state_flag_tree <- function(tree, metadata, image_column = "ImagePath", label
     left_join(metadata, by = setNames(label_column, "label"))
 
   # create "offset" to space flag icons at end of tip label, to prevent overlap
-  offset <- max(tree_data$x, na.rm = TRUE) * 0.085
+  offset <- max(tree_data$x, na.rm = TRUE) * 0.15
 
   # create ggtree plot
   p +
@@ -19,5 +19,5 @@ us_state_flag_tree <- function(tree, metadata, image_column = "ImagePath", label
       aes(x = x + offset, y = y, image = .data[[image_column]]),
       size = 0.025, asp = 1.0
     ) +
-    geom_tiplab()
+    geom_tiplab(size = 3)
 }
