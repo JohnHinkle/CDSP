cd7f9c82652632ff673b8bd2774ce414717c67da
John Hinkle <john.hinkle@utexas.edu>
2025-04-28 10:48:41 -0500

    functionality

--- R/to_image.R
+++ R/to_image.R
@@ -1,4 +1,3 @@
-to_image <- function(data, column_name = "state", new_dir = "images"){
-  dir.create(new_dir)
+to_image <- function(data, column_name = "state", new_dir = "states"){
   dplyr::mutate(data, ImagePath = paste0(new_dir, "/", .data[[column_name]], ".png"))
 }
