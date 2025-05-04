983061233e335f2ef52393e545908853497b84fb
John Hinkle <john.hinkle@utexas.edu>
2025-05-04 11:19:42 -0500

    specify packages

--- R/us_state_flag_tree.R
+++ R/us_state_flag_tree.R
@@ -7,7 +7,7 @@
 #' @keywords tree
 #' @export
 #' @examples
-#' us_state_flag_tree <- function(tree = t, metadata = d, image_column = "image_path", label_column = "label")
+#' us_state_flag_tree(tree = t, metadata = d, image_column = "image_path", label_column = "label")
 us_state_flag_tree <- function(tree, metadata, image_column = "image_path", label_column = "label") {
 
   # create ggtree object from tree object
@@ -16,8 +16,8 @@ us_state_flag_tree <- function(tree, metadata, image_column = "image_path", labe
   # extract tree data and join with metadata
   tree_data <- p$data
   tip_data <- tree_data |>
-    filter(isTip) |>
-    left_join(metadata, by = setNames(label_column, "label"))
+    dplyr::filter(isTip) |>
+    dplyr::left_join(metadata, by = setNames(label_column, "label"))
 
   # create "offset" to space flag icons at end of tip label, to prevent overlap
   offset <- max(tree_data$x, na.rm = TRUE) * 0.15
